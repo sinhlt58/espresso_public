edd2511cb53d844c0a2cb45a63785ead72463f2b
conganhpham <conganhphamnd@gmail.com>
2018-12-05 14:07:11 +0700

    update map data by morphia for get fast url

---
+++ libs_spec/storm_crawler/core/src/main/java/com/digitalpebble/stormcrawler/mongodb/services/LegalUrlService.java
@@ -0,0 +1,30 @@
+package com.digitalpebble.stormcrawler.mongodb.services;
+
+import com.digitalpebble.stormcrawler.mongodb.models.LegalUrlEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import xyz.morphia.query.Query;
+
+import java.util.List;
+
+public class LegalUrlService extends BaseService{
+
+    public static final Logger LOG = LoggerFactory
+            .getLogger(LegalUrlService.class);
+    public static List<LegalUrlEntity> getRulesByHost(String hostname){
+        Query<LegalUrlEntity> query = datastore.createQuery(LegalUrlEntity.class);
+
+        String global = "GLOBAL";
+        String domainScope = "domain:" + hostname;
+        String hostScope = "host:" + hostname;
+        String metadataScope = "metadata:" + hostname;
+        query.or(
+            query.criteria("scope").equal(global),
+            query.criteria("scope").equal(domainScope),
+            query.criteria("scope").equal(hostScope),
+            query.criteria("scope").equal(metadataScope)
+        );
+        return query.asList();
+    }
+
+}
