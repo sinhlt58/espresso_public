620bf0aae8e1c9449b17a9f9c57950b1aae921b8
sinhlt58 <sinhlt58@gmail.com>
2018-12-05 17:41:56 +0700

    set up analytic topology #38

---
+++ nlp_analytics/src/main/java/com/uet/nlp/nlp_analytics/SplitReviewBolt.java
@@ -0,0 +1,52 @@
+package com.uet.nlp.nlp_analytics;
+
+import java.util.Map;
+
+import com.uet.nlp.common.Document;
+
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.IRichBolt;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SplitReviewBolt implements IRichBolt {
+
+    private static final Logger LOG = LoggerFactory
+            .getLogger(SplitReviewBolt.class);
+
+    private OutputCollector _collector;
+
+    @Override
+    public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+        _collector = collector;
+    }
+
+    @Override
+    public void execute(Tuple input) {
+        System.out.println(input);
+        String docId = (String) input.getValueByField("docId");
+        Document doc = (Document) input.getValueByField("doc");
+
+        LOG.info("docId: {}", docId);
+    }
+
+    @Override
+    public void cleanup() {
+
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields("docId", "doc"));
+	}
+
+	@Override
+	public Map<String, Object> getComponentConfiguration() {
+		return null;
+	}
+
+}
