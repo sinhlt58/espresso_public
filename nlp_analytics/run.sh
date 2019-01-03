5185b65ddb68dedc67d64c1664c21773d6edba0e
sinhlt58 <sinhlt58@gmail.com>
2019-01-03 15:18:40 +0700

    more managable sciprts for nlp_analytics project

--- nlp_analytics/run.sh
+++ nlp_analytics/run.sh
@@ -7,14 +7,15 @@ then
     mvn clean package
 fi
 
+# Analysis from crawling data START
 if [ "$1" = "index" ]
 then
-    ./es_index.sh
+    ./es_index.sh v2_analysis
 fi
 
-if [ "$1" = "update_analysis_status" ]
+if [ "$1" = "reset_analysis_status" ]
 then
-    ./update_analysis_status.sh
+    ./update_analysis_status.sh v2_index analysis_status _done
 fi
 
 if [ "$1" = "local" ]
@@ -26,23 +27,26 @@ if [ "$1" = "remote" ]
 then
     storm jar target/nlp_analytics-1.0-SNAPSHOT.jar  org.apache.storm.flux.Flux --remote analytics.yaml
 fi
+# Analysis from crawling data END
 
-if [ "$1" = "nlp_index" ]
+# Sentiment nlp START
+if [ "$1" = "sentiment_index" ]
 then
-    ./es_sentiment.sh
+    ./es_index.sh v2_sentiment
 fi
 
-if [ "$1" = "nlp_update_sentiment_status" ]
+if [ "$1" = "reset_sentiment_status" ]
 then
-    ./update_sentiment.sh
+    ./update_analysis_status.sh v2_analysis sentiment_status _done
 fi
 
-if [ "$1" = "nlp_local" ]
+if [ "$1" = "sentiment_local" ]
 then
-    storm jar target/nlp_analytics-1.0-SNAPSHOT.jar  org.apache.storm.flux.Flux --local nlp.local.yaml --sleep 864000000
+    storm jar target/nlp_analytics-1.0-SNAPSHOT.jar  org.apache.storm.flux.Flux --local sentiment.local.yaml --sleep 864000000
 fi
 
-if [ "$1" = "nlp_remote" ]
+if [ "$1" = "sentiment_remote" ]
 then
-    storm jar target/nlp_analytics-1.0-SNAPSHOT.jar  org.apache.storm.flux.Flux --remote nlp.yaml
+    storm jar target/nlp_analytics-1.0-SNAPSHOT.jar  org.apache.storm.flux.Flux --remote sentiment.yaml
 fi
+# Sentiment nlp END
--- nlp_analytics/update_analysis_status.sh
+++ nlp_analytics/update_analysis_status.sh
@@ -1,11 +1,14 @@
 ESHOST="http://localhost:9200"
 ESCREDENTIALS="-u elastic:passwordhere"
+index_name=$1
+status_field_name=$2
+status_done_value=$3
 
-echo "Change analysis_status for all documents in index"
+echo "Change $status_field_name field for to $status_done_value all documents in $index_name"
 
-curl $ESCREDENTIALS -s -XPOST $ESHOST/v2_index/_update_by_query?conflicts=proceed -H 'Content-Type: application/json' -d '
+curl $ESCREDENTIALS -s -XPOST $ESHOST/$index_name/_update_by_query?conflicts=proceed -H 'Content-Type: application/json' -d '
 {
 	"script": {
-    	"source": "ctx._source[\"analysis_status\"] = \"_done\""
+    	"source": "ctx._source[\"'$status_field_name'\"] = \"'$status_done_value'\""
   	}
 }'
