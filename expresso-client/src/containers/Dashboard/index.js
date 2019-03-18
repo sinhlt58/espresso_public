547d26ff0e563eada1cd44a3eaf5c7ff5c735c79
Trung Duc <caoboiyb1997@gmail.com>
2019-03-18 12:12:01 +0700

    add voice2text

--- expresso-client/src/containers/Dashboard/index.js
+++ expresso-client/src/containers/Dashboard/index.js
@@ -1,18 +1,32 @@
-import React, { Component } from 'react';
-import { Input, AutoComplete, Tag, Row, Col, message } from 'antd';
-import { Link } from 'react-router-dom';
-import Wrapper from '../../hoc/Wrapper';
+import React, { Component } from "react";
+import {
+  Input,
+  AutoComplete,
+  Tag,
+  Row,
+  Col,
+  message,
+  Button,
+  Icon
+} from "antd";
+import SpeechRecognition from "react-speech-recognition";
+import { Link } from "react-router-dom";
+import Wrapper from "../../hoc/Wrapper";
 import {
   brandAutocomplete,
   getAppStats,
   getPopuplarBrands,
-  getBadBrands,
-} from '../../graphql-client/api';
-import axios from 'axios';
-import moment from 'moment';
-import './style.css';
+  getBadBrands
+} from "../../graphql-client/api";
+import axios from "axios";
+import moment from "moment";
+import "./style.css";
 
 const Search = Input.Search;
+
+const options = {
+  autoStart: false
+};
 
 class Dashboard extends Component {
   state = {
@@ -26,6 +40,8 @@ class Dashboard extends Component {
     dealers: [],
     badBrands: [],
     badDealers: [],
+    listening: false,
+    strValue: ""
   };
 
   async componentDidMount() {
@@ -36,14 +52,14 @@ class Dashboard extends Component {
         brands_count,
         comments_count,
         products_count,
-        domain_count,
+        domain_count
       } = res.data.getSummaryApp;
 
       this.setState({
         brands_count: Number(brands_count).toLocaleString(),
         comments_count: Number(comments_count).toLocaleString(),
         products_count: Number(products_count).toLocaleString(),
-        domain_count: Number(domain_count).toLocaleString(),
+        domain_count: Number(domain_count).toLocaleString()
       });
     }
 
@@ -62,94 +78,131 @@ class Dashboard extends Component {
     }
   }
 
-  _onInput = async (text) => {
-    if (text.trim() !== '') {
+  _onInput = async text => {
+    this.stopVoice();
+    if (text.trim() !== "") {
+      await this.setState({
+        strValue: text
+      });
       const res = await brandAutocomplete(text.toLowerCase());
       this.setState({
-        completion: res.data.brandCompletion,
+        completion: res.data.brandCompletion
       });
     } else {
       this.setState({
-        completion: [],
+        completion: []
       });
     }
   };
 
-  _onSelectSuggester = (text) => {
+  _onSelectSuggester = text => {
     this._onSearch(text);
   };
 
-  _onSearch = async (value) => {
-    const res = await brandAutocomplete(value.toLowerCase());
+  _onSearch = async value => {
+    let res;
+    this.stopVoice();
+    if (this.state.strValue === "") {
+      res = await brandAutocomplete(this.props.transcript);
+    } else {
+      res = await brandAutocomplete(this.state.strValue.toLowerCase());
+    }
 
     if (res.data.brandCompletion.length === 0) {
       const botRes = await axios.post(
-        'https://chatbot.sachmem.vn/api/v1/bots/data_espresso/chat',
+        "https://chatbot.sachmem.vn/api/v1/bots/data_espresso/chat",
         {
-          text: value,
-        },
+          text: value
+        }
       );
 
       if (botRes.data.rasa_intent.intent.name === null) {
-        message.error('Không tìm thấy thương hiệu');
-      } else if (botRes.data.rasa_intent.intent.name === 'tim_bl_xau') {
+        message.error("Không tìm thấy thương hiệu");
+      } else if (botRes.data.rasa_intent.intent.name === "tim_bl_xau") {
         const entities = botRes.data.rasa_ner.merged_entities;
         const brand = entities[0].value;
         if (entities.length > 1) {
           const time = entities[1].value;
           this.props.history.push({
             pathname: `/analytics/${brand}`,
-            state: { time, optionsSort: 'ASC' },
+            state: { time, optionsSort: "ASC" }
           });
         } else {
           this.props.history.push({
             pathname: `/analytics/${brand}`,
-            state: { optionsSort: 'ASC' },
+            state: { optionsSort: "ASC" }
           });
         }
-      } else if (botRes.data.rasa_intent.intent.name === 'so_sanh') {
+      } else if (botRes.data.rasa_intent.intent.name === "so_sanh") {
         const brand1 = botRes.data.rasa_ner.merged_entities[0].value;
         const brand2 = botRes.data.rasa_ner.merged_entities[1].value;
 
         this.props.history.push({
           pathname: `/compare`,
-          state: { brand1, brand2 },
+          state: { brand1, brand2 }
         });
-      } else if (botRes.data.rasa_intent.intent.name === 'tim_thuong_hieu') {
+      } else if (botRes.data.rasa_intent.intent.name === "tim_thuong_hieu") {
         const entities = botRes.data.rasa_ner.merged_entities;
         const product = entities[entities.length - 1].value;
 
         this.props.history.push({
           pathname: `/products`,
-          state: { product },
+          state: { product }
         });
-      } else if (botRes.data.rasa_intent.intent.name === 'phan_tich_tu_khoa') {
+      } else if (botRes.data.rasa_intent.intent.name === "phan_tich_tu_khoa") {
         const brand = botRes.data.rasa_ner.merged_entities[0].value;
 
         this.props.history.push({
-          pathname: `/analytics/${brand}`,
+          pathname: `/analytics/${brand}`
         });
       }
     } else {
-      if (value.trim() === '' || value === undefined) {
-        alert('Vui lòng nhập tên thương hiệu trước khi tiếp tục');
+      if (value.trim() === "" || value === undefined) {
+        alert("Vui lòng nhập tên thương hiệu trước khi tiếp tục");
       } else {
         this.props.history.push({
-          pathname: `/analytics/${value}`,
+          pathname: `/analytics/${value}`
         });
       }
     }
   };
 
+  _onListen = async () => {
+    if (this.state.listening === true) {
+      this.stopVoice();
+    } else {
+      await this.setState({
+        listening: true,
+        strValue: ""
+      });
+      this.props.startListening();
+    }
+  };
+
+  stopVoice = async () => {
+    await this.setState({
+      listening: false,
+      strValue: this.props.transcript
+    });
+    this.props.abortListening();
+    this.props.resetTranscript();
+  };
+
   render() {
+    const { browserSupportsSpeechRecognition, transcript } = this.props;
+
+    if (!browserSupportsSpeechRecognition) {
+      return null;
+    }
+
     return (
       <Wrapper isHome location={this.props.location.pathname}>
         <div
           style={{
-            textAlign: 'center',
+            textAlign: "center",
             marginTop: 80,
-            width: '100%',
-            marginBottom: 50,
+            width: "100%",
+            marginBottom: 50
           }}
         >
           <h1>Nhập tên thương hiệu muốn phân tích</h1>
@@ -160,6 +213,9 @@ class Dashboard extends Component {
             onSelect={this._onSelectSuggester}
             onSearch={this._onInput}
             optionLabelProp="value"
+            value={
+              this.state.strValue === "" ? transcript : this.state.strValue
+            }
           >
             <Search
               className="search-dashboard"
@@ -167,15 +223,23 @@ class Dashboard extends Component {
               onSearch={this._onSearch}
               enterButton
               size="large"
+              prefix={
+                <Button
+                  type={this.state.listening ? "primary" : "normal"}
+                  onClick={this._onListen}
+                >
+                  <Icon type="notification" />
+                </Button>
+              }
             />
           </AutoComplete>
         </div>
-        <div style={{ paddingLeft: '20%', paddingBottom: 30 }}>
+        <div style={{ paddingLeft: "20%", paddingBottom: 30 }}>
           <h3>Tìm kiếm nâng cao:</h3>
           <Link
             to={{
               pathname: `/analytics/oem`,
-              state: { optionsSort: 'ASC' },
+              state: { optionsSort: "ASC" }
             }}
           >
             <a href="#">Tìm bình luận xấu về oem</a>
@@ -185,14 +249,14 @@ class Dashboard extends Component {
             to={{
               pathname: `/analytics/everest`,
               state: {
-                optionsSort: 'ASC',
+                optionsSort: "ASC",
                 time: {
                   from: moment()
-                    .subtract(1, 'month')
-                    .format('YYYY-MM-DD'),
-                  to: moment().format('YYYY-MM-DD'),
-                },
-              },
+                    .subtract(1, "month")
+                    .format("YYYY-MM-DD"),
+                  to: moment().format("YYYY-MM-DD")
+                }
+              }
             }}
           >
             <a href="#">Bình luận xấu về everest trong 1 tháng trước</a>
@@ -201,7 +265,7 @@ class Dashboard extends Component {
           <Link
             to={{
               pathname: `/compare`,
-              state: { brand1: 'nike', brand2: 'adidas' },
+              state: { brand1: "nike", brand2: "adidas" }
             }}
           >
             <a href="#">So sánh nike và adidas</a>
@@ -210,7 +274,7 @@ class Dashboard extends Component {
           <Link
             to={{
               pathname: `/products`,
-              state: { product: 'giày nam' },
+              state: { product: "giày nam" }
             }}
           >
             <a href="#">Tìm shop bán giày nam</a>
@@ -218,7 +282,7 @@ class Dashboard extends Component {
           <br />
           <Link
             to={{
-              pathname: `/analytics/everest`,
+              pathname: `/analytics/everest`
             }}
           >
             <a href="#">Phân tích everest</a>
@@ -227,9 +291,9 @@ class Dashboard extends Component {
         <div
           style={{
             paddingTop: 50,
-            backgroundColor: 'white',
-            textAlign: 'center',
-            paddingBottom: 50,
+            backgroundColor: "white",
+            textAlign: "center",
+            paddingBottom: 50
           }}
         >
           <h2>Phần mềm giúp đo lường sức khỏe thương hiệu</h2>
@@ -257,7 +321,7 @@ class Dashboard extends Component {
             <h2 style={{ marginTop: 20, marginBottom: 20 }}>
               Các thương hiệu phổ biến nhất
             </h2>
-            {this.state.brands.map((item) => (
+            {this.state.brands.map(item => (
               <Tag color="#87d068">
                 <Link to={`/analytics/${item}`}>{item}</Link>
               </Tag>
@@ -265,7 +329,7 @@ class Dashboard extends Component {
             <h2 style={{ marginTop: 20, marginBottom: 20 }}>
               Các cửa hàng phổ biến nhất
             </h2>
-            {this.state.dealers.map((item) => (
+            {this.state.dealers.map(item => (
               <Tag color="#87d068">
                 <Link to={`/analytics/${item}`}>{item}</Link>
               </Tag>
@@ -273,7 +337,7 @@ class Dashboard extends Component {
             <h2 style={{ marginTop: 20, marginBottom: 20 }}>
               Các thương hiệu kém nhất
             </h2>
-            {this.state.badBrands.map((item) => (
+            {this.state.badBrands.map(item => (
               <Tag color="#f50">
                 <Link to={`/analytics/${item}`}>{item}</Link>
               </Tag>
@@ -281,7 +345,7 @@ class Dashboard extends Component {
             <h2 style={{ marginTop: 20, marginBottom: 20 }}>
               Các cửa hàng kém nhất
             </h2>
-            {this.state.badDealers.map((item) => (
+            {this.state.badDealers.map(item => (
               <Tag color="#f50">
                 <Link to={`/analytics/${item}`}>{item}</Link>
               </Tag>
@@ -293,4 +357,4 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard;
+export default SpeechRecognition(options)(Dashboard);
