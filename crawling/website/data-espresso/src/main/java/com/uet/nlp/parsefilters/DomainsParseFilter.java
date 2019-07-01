3c0bd733fa5969ba74514321ebd69bdd699c9734
phamconganh <conganhphamnd@gmail.com>
2019-07-01 16:07:32 +0700

    fix nested ul

--- crawling/website/data-espresso/src/main/java/com/uet/nlp/parsefilters/DomainsParseFilter.java
+++ crawling/website/data-espresso/src/main/java/com/uet/nlp/parsefilters/DomainsParseFilter.java
@@ -44,11 +44,11 @@ public class DomainsParseFilter extends ParseFilter {
 
             if (type == RuleType.OWNTEXT) {
                 selectorExpression = selectorExpression.replace("rule_own_text", "");
-            } else if (type == RuleType.JSON_SCRIPT){
+            } else if (type == RuleType.JSON_SCRIPT) {
                 selectorExpression = selectorExpression.replace("rule_json_script", "");
-            } else if(type == RuleType.INNER_HTML) {
+            } else if (type == RuleType.INNER_HTML) {
                 selectorExpression = selectorExpression.replace("rule_inner_html", "");
-            } else if(type == RuleType.NESTED) {
+            } else if (type == RuleType.NESTED) {
                 selectorExpression = selectorExpression.replace("rule_nested_ul", "");
             }
         }
@@ -125,7 +125,7 @@ public class DomainsParseFilter extends ParseFilter {
         public ArrayList<String> evaluateJsonScript(Document docJsoup) {
             String[] tokens = selectorExpression.split("rootpath");
             String selector = tokens[0].trim();
-            String path     = tokens[1].trim();
+            String path = tokens[1].trim();
 
             Elements els = docJsoup.select(selector);
             ArrayList<String> res = new ArrayList<>();
@@ -135,7 +135,7 @@ public class DomainsParseFilter extends ParseFilter {
                     data = data.replaceAll("<!--.*?-->", "");
                     JsonNode root = mapper.readTree(data);
                     JsonNode node = root.at(path);
-                    if(!node.isMissingNode()){
+                    if (!node.isMissingNode()) {
                         res.add(node.textValue());
                     }
                 } catch (Exception exception) {
@@ -152,16 +152,17 @@ public class DomainsParseFilter extends ParseFilter {
             Elements els = docJsoup.select(selector);
             ArrayList<String> res = new ArrayList<>();
             for (Element e : els) {
-                if(tokens.length > 1){
-                    for(int i=0; i<tokens.length; i++) {
+                if (tokens.length > 1) {
+                    for (int i = 0; i < tokens.length; i++) {
                         Elements removeElements = e.select(tokens[i].trim());
-                        if(removeElements != null){
-                           removeElements.remove();
+                        if (removeElements != null) {
+                            removeElements.remove();
                         }
                     }
                 }
                 String innerHtml = e.html();
-                if(innerHtml != null && innerHtml.trim().length() > 0) res.add(innerHtml);
+                if (innerHtml != null && innerHtml.trim().length() > 0)
+                    res.add(innerHtml);
             }
             return res;
         }
@@ -174,15 +175,48 @@ public class DomainsParseFilter extends ParseFilter {
             for (Element e : els) {
                 arrayRoot.add(buildJsonNested(e, true));
             }
-            if(arrayRoot.size()>0) res.add((arrayRoot.toString()));
+            if (arrayRoot.size() > 0)
+                res.add((arrayRoot.toString()));
             return res;
 
         }
 
         private ObjectNode buildJsonNested(Element e, boolean isFisrt) {
+
             ObjectNode node = mapper.createObjectNode();
-            String spec_header = e.child(0).text();
+            String spec_header = "";
+            Element subUl = null;
+            ArrayNode arrayNodes = mapper.createObjectNode().arrayNode();
+
+            if (isFisrt == true && e.children().size() == 1) {
+
+                Element ul = e.child(0);
+                Element li = ul.child(0);
+                spec_header = li.child(0).text().trim();
+
+                subUl = li.selectFirst("ul");
+
+            } else {
+
+                Element head = e.child(0);
+                spec_header = head.text().trim();
+
+                subUl = e.selectFirst("ul");
+
+            }
+
             node.put("name", spec_header);
+
+            if (subUl != null) {
+                for (int i = 0; i < subUl.children().size(); i++) {
+                    Element subLi = subUl.child(i);
+                    arrayNodes.add(buildJsonNested(subLi, false));
+                }
+            }
+            if (arrayNodes.size() > 0) node.put("children", arrayNodes);
+
+            // String spec_header = e.child(0).text();
+            // node.put("name", spec_header);
 
             // if(e.children().size() > 1){
             //     Elements lis = e.child(1).children();
@@ -192,20 +226,6 @@ public class DomainsParseFilter extends ParseFilter {
             //     }
             //     if(arrayNode.size() > 0) node.put("children", arrayNode);
             // }
-
-            // Lay header cua phan tu dau tien lam 'name' neu trong ul cha khong co 1 the khac lam tieu de
-            Elements lis = null;
-            if(e.children().size() > 1 && isFisrt == false){
-                lis = e.child(1).children();
-            } else {
-                lis = e.child(0).children();
-            }
-            // ???
-            ArrayNode arrayNode = mapper.createObjectNode().arrayNode();
-            for (Element li : lis) {
-                arrayNode.add(buildJsonNested(li, false));
-            }
-            if(arrayNode.size() > 0) node.put("children", arrayNode);
 
             return node;
 
@@ -218,12 +238,12 @@ public class DomainsParseFilter extends ParseFilter {
 
             for (String selector : selectors) {
                 Elements els = docJsoup.select(selector.trim());
-                if (els != null && !els.isEmpty()){
+                if (els != null && !els.isEmpty()) {
                     values.add(els.first().text());
                 }
             }
             String joinedValue = String.join(" ", values);
-            if (!joinedValue.isEmpty()){
+            if (!joinedValue.isEmpty()) {
                 res.add(joinedValue);
             }
             return res;
@@ -232,7 +252,7 @@ public class DomainsParseFilter extends ParseFilter {
         public ArrayList<String> evaluateAttribute(Document docJsoup) {
             String[] tokens = selectorExpression.split("@");
             String selector = tokens[0].trim();
-            String attr     = tokens[1].trim();
+            String attr = tokens[1].trim();
 
             Elements els = docJsoup.select(selector);
             ArrayList<String> res = new ArrayList<>();
@@ -260,7 +280,7 @@ public class DomainsParseFilter extends ParseFilter {
             Document jsoupDoc = (Document) metadata.getObjectValue("jsoupDoc");
 
             // for each domain
-            for (String domainName : domainFieldRulesMap.keySet()){
+            for (String domainName : domainFieldRulesMap.keySet()) {
                 Map<String, ArrayList<CustomRule>> fieldRulesMap = domainFieldRulesMap.get(domainName);
 
                 // for each field
@@ -294,35 +314,35 @@ public class DomainsParseFilter extends ParseFilter {
                 }
             }
 
-        } catch (Exception error){
+        } catch (Exception error) {
             LOG.error("Error filter element parent of: {} , error: {}", URL, error);
         }
 
         metadata.remove("html");
     }
 
-    public void getRules(String host){
+    public void getRules(String host) {
         List<DomainEntity> domainEntities = DomainService.getDatasByHost(host);
-        for (DomainEntity domainEntity :domainEntities) {
+        for (DomainEntity domainEntity : domainEntities) {
             String domainKey = domainEntity.getEsname();
-            //LOG.info("Domain name: {}", domainKey);
+            // LOG.info("Domain name: {}", domainKey);
 
             ArrayList<Rules> rules = domainEntity.getRules();
 
             Map<String, ArrayList<CustomRule>> fieldRulesMap = new HashMap<>();
             domainFieldRulesMap.put(domainKey, fieldRulesMap);
 
-            for (Rules rule:rules) {
+            for (Rules rule : rules) {
                 String fieldKey = rule.getLabel();
-                //LOG.info("Field: {}", fieldKey);
+                // LOG.info("Field: {}", fieldKey);
 
                 ArrayList<CustomRule> selectorList = new ArrayList<>();
                 String ruleData = rule.getRule();
                 List<String> selectors = Arrays.asList(ruleData.split(", "));
 
                 for (String selector : selectors) {
-                    //LOG.info("Selector: {}", selector);
-                    if (selector.length() > 0){
+                    // LOG.info("Selector: {}", selector);
+                    if (selector.length() > 0) {
                         selectorList.add(new CustomRule(selector));
                     }
                 }
